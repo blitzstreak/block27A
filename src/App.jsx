fd086e0bb048d76b32c293352476c66a5c0b431f
Andy Yu <andy.chenyuhuang@gmail.com>
2024-05-13 14:24:09 -0400

    newcommit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+import './index.css';
+import PrimaryDashboard from './components/PrimaryDashboard';
+function App() {
+
+  return (
+    <PrimaryDashboard />
+  );
+}
+
+export default App;
